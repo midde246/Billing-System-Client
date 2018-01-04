7109e1e75fdf1a03f7926519a10bc28a9d085cc2
midde246 <spmidde@gmail.com>
2018-01-04 21:18:20 +0530

    first commit from my laptop

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,12 +5,31 @@ import 'rxjs/add/operator/map'
 @Injectable()
 
 export class ApiService{
-    constructor(private http:Http){}
+    token:string = null
+    constructor(private http:Http){
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'))
+        this.token = currentUser.token
+    }
 
-    sendData(url:any, data:any){
+    signIn(url:any, data:any){
         return this.http.post(url, data).map(
-                    (response:Response) => response.json()
+                (response:Response) => {
+                   let token = response.json().token
+                   if(token){
+                       this.token = token
+                       localStorage.setItem('currentUser',JSON.stringify({user: data.username, token:token}))
+                       return true
+                   }else{
+                       return false
+                   }
+                }
               )
+    }
+
+    signout(){
+        //Clear the authentication data....
+        this.token = null
+        localStorage.removeItem('currentUser')
     }
 
 }
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 
@@ -10,20 +11,31 @@ import { ApiService } from '../services/api.service';
 })
 
 export class SigninComponent{
-    constructor(private apiService: ApiService){
+    constructor(private apiService: ApiService,private router:Router){
 
     }
-    
+     
+    ngOnInit(){
+        this.apiService.signout()
+    }
+   
+    private error:any
     userSigninForm = new FormGroup({
         username: new FormControl(),
         password: new FormControl()
     })
+
     Data2Login(){
-         this.apiService.sendData('http://127.0.0.1:8080/api/testLogin', this.userSigninForm.value).subscribe(
-             resData => console.log(resData)
+         this.apiService.signIn('http://127.0.0.1:8080/api/testLogin', this.userSigninForm.value).subscribe(
+             result => {
+                 if(result == true){
+                    this.router.navigate(['/dashboard'])
+                 }
+                 else{
+                     this.error = 'Wrong Username or Password'
+                 }
+             }
          )
-    }
-   
-    
+    } 
      
 } 
